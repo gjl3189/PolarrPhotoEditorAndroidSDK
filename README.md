7a3c1b0aa221e921055e1efbcf87d74826547305
Derek Yan <derek@DN0a226210.SUNet>
2017-06-29 12:26:06 -0700

    sdk tools

---
+++ README.md
@@ -0,0 +1,103 @@
+# Polarr Photo Editor Android SDK
+The Polarr Photo Editor Android SDK exposes a subset of the native OpenGL rendering APIs used by Polarr Photo Editor. This SDK includes a starter project (app) that calls the Android SDK and users can try out the available editing tools. This repo is intended to demonstrate a subset of the capabilities of the full Android SDK. 
+
+The minimum Android API Level is 14 (4.0.3).
+
+The SDK in this repo expires on September 30, 2017. For pricing and more info regarding the full license SDK, please email info@polarr.co.
+
+## Init GLRenderView
+```xml
+<co.polarr.renderer.render.GLRenderView
+  android:id="@+id/render_view"
+  android:layout_width="match_parent"
+  android:layout_height="match_parent"/>
+```
+```java
+GLRenderView renderView = (GLRenderView) findViewById(R.id.render_view);
+```
+## Import image
+```java
+renderView.importImage(BitmapFactory.decodeResource(getResources(), R.mipmap.demo));
+```
+## Global adjustments
+Adjust a specific adjustment with value. The value `from -1.0f to +1.0f`.
+More adjustment lables, see [Basic global adjustments](#basic-global-adjustments)
+```java
+String label = "contrast";
+float adjustmentValue = 0.5f;
+renderView.updateShader(label, adjustmentValue);
+renderView.requestRender();
+```
+## Show original image
+```java
+boolean isOriginal = true;
+renderView.showOriginal(isOriginal);
+```
+## Reset image
+Reset image to original.
+```java
+renderView.resetAll();
+```
+## Export image
+```java
+// save byte array to a file with PNG format. If need a bitmap, set needImage to true.
+boolean needImage = true;
+renderView.exportImageData(needImage, new OnExportCallback() {
+    @Override
+    public void onExport(Bitmap bitmap, byte[] array) {
+        // save array to a file
+    }
+});
+```
+## Basic global adjustments
+```
+"exposure",
+"contrast",
+"saturation",
+"distortion_horizontal",
+"distortion_vertical",
+"fringing",
+"color_denoise",
+"luminance_denoise",
+"dehaze",
+"diffuse",
+"temperature",
+"tint",
+"gamma",
+"highlights",
+"shadows",
+"whites",
+"blacks",
+"clarity",
+"vibrance",
+"highlights_hue",
+"highlights_saturation",
+"shadows_hue",
+"shadows_saturation",
+"balance",
+"sharpen",
+"hue_red",
+"hue_orange",
+"hue_yellow",
+"hue_green",
+"hue_aqua",
+"hue_blue",
+"hue_purple",
+"hue_magenta",
+"saturation_red",
+"saturation_orange",
+"saturation_yellow",
+"saturation_green",
+"saturation_aqua",
+"saturation_blue",
+"saturation_purple",
+"saturation_magenta",
+"luminance_red",
+"luminance_orange",
+"luminance_yellow",
+"luminance_green",
+"luminance_aqua",
+"luminance_blue",
+"luminance_purple",
+"luminance_magenta",
+```
---
+++ app/src/main/java/co/polarr/polarrrenderdemo/MainActivity.java
@@ -0,0 +1,348 @@
+package co.polarr.polarrrenderdemo;
+
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import co.polarr.renderer.render.GLRenderView;
+import co.polarr.renderer.render.OnExportCallback;
+import co.polarr.utils.ImageLoadUtil;
+import co.polarr.utils.Logger;
+import co.polarr.utils.ThreadManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_IMPORT_PHOTO = 1;
+    private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 1;
+    private AppCompatSeekBar seekbar;
+    private TextView labelTv;
+
+    /**
+     * Render View
+     */
+    private GLRenderView renderView;
+    /**
+     * adjustment container
+     */
+    private View sliderCon;
+    /**
+     * save adjustment values
+     */
+    private Map<String, Integer> localStateMap = new HashMap<>();
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    hideAll();
+                    showMenu();
+                    return false;
+                case R.id.navigation_list:
+                    showList();
+                    return false;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        // init render view
+        renderView = (GLRenderView) findViewById(R.id.render_view);
+
+        // add on touch event to show original image
+        renderView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getActionMasked()) {
+                    case MotionEvent.ACTION_DOWN:
+                        renderView.showOriginal(true);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        renderView.showOriginal(false);
+                        break;
+                    default:
+                        return false;
+                }
+
+                return true;
+            }
+        });
+
+        // post init render view
+        ThreadManager.executeOnMainThread(new Runnable() {
+            @Override
+            public void run() {
+                initRenderView();
+            }
+        });
+
+        sliderCon = findViewById(R.id.slider);
+        sliderCon.setVisibility(View.GONE);
+
+        labelTv = (TextView) findViewById(R.id.label_tv);
+        seekbar = (AppCompatSeekBar) findViewById(R.id.seekbar);
+
+    }
+
+    private void importImageDemo() {
+        renderView.importImage(BitmapFactory.decodeResource(getResources(), R.mipmap.demo));
+    }
+
+    private void importImage() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMPORT_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (REQUEST_IMPORT_PHOTO == requestCode) {
+            if (data != null) {
+                Uri uri = data.getData();
+                int orientation = ImageLoadUtil.getOrientation(this, uri);
+                int maxTextureSize = ImageLoadUtil.getMaxTextureSize();
+                Bitmap imageBm = ImageLoadUtil.decodeThumbBitmapFromUrl(this, uri, maxTextureSize, maxTextureSize, orientation);
+                renderView.importImage(imageBm);
+            }
+        }
+    }
+
+    private void exportImage() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE);
+
+            return;
+        }
+
+        renderView.exportImageData(false, new OnExportCallback() {
+            @Override
+            public void onExport(Bitmap bitmap, byte[] array) {
+                try {
+                    String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                    File storageDirectory = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "PolarrDemo");
+
+                    //Creating the storage directory if it doesn't exist
+                    if (!storageDirectory.exists()) {
+                        storageDirectory.mkdirs();
+                    }
+
+                    //Creating the temporary storage file
+                    File targetImagePath = File.createTempFile(timeStamp + "_", ".jpg", storageDirectory);
+
+                    OutputStream outputStream = new FileOutputStream(targetImagePath);
+
+                    outputStream.write(array);
+                    outputStream.close();
+
+                    //Rescanning the icon_library/gallery so it catches up with our own changes
+                    Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    mediaScanIntent.setData(Uri.fromFile(targetImagePath));
+                    sendBroadcast(mediaScanIntent);
+
+                    Toast.makeText(MainActivity.this, String.format(Locale.ENGLISH, "Saved to: %s", targetImagePath.getAbsolutePath()), Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Logger.e("Cannot save exporting file to disk (" + e.toString() + ").");
+                }
+            }
+        });
+    }
+
+    private void reset() {
+        renderView.resetAll();
+    }
+
+    private void initRenderView() {
+        renderView.initRender();
+        renderView.setRenderBackgroundColor(Color.WHITE);
+    }
+
+    private void showList() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        final CharSequence items[] = new CharSequence[]{
+                "exposure",
+                "contrast",
+                "saturation",
+                "distortion_horizontal",
+                "distortion_vertical",
+                "fringing",
+                "color_denoise",
+                "luminance_denoise",
+                "dehaze",
+                "diffuse",
+                "temperature",
+                "tint",
+                "gamma",
+                "highlights",
+                "shadows",
+                "whites",
+                "blacks",
+                "clarity",
+                "vibrance",
+                "highlights_hue",
+                "highlights_saturation",
+                "shadows_hue",
+                "shadows_saturation",
+                "balance",
+                "sharpen",
+                "hue_red",
+                "hue_orange",
+                "hue_yellow",
+                "hue_green",
+                "hue_aqua",
+                "hue_blue",
+                "hue_purple",
+                "hue_magenta",
+                "saturation_red",
+                "saturation_orange",
+                "saturation_yellow",
+                "saturation_green",
+                "saturation_aqua",
+                "saturation_blue",
+                "saturation_purple",
+                "saturation_magenta",
+                "luminance_red",
+                "luminance_orange",
+                "luminance_yellow",
+                "luminance_green",
+                "luminance_aqua",
+                "luminance_blue",
+                "luminance_purple",
+                "luminance_magenta",
+        };
+
+
+        adb.setItems(items, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int n) {
+                sliderCon.setVisibility(View.VISIBLE);
+                final CharSequence label = items[n];
+                labelTv.setText(label);
+                seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        float adjustmentValue = (float) progress / 100f * 2f - 1f;
+                        renderView.updateShader(label.toString(), adjustmentValue);
+                        renderView.requestRender();
+
+                        localStateMap.put(label.toString(), progress);
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                });
+                if (localStateMap.containsKey(label.toString())) {
+
+                    seekbar.setProgress(localStateMap.get(label.toString()));
+                } else {
+                    seekbar.setProgress(50);
+                }
+
+                dialog.dismiss();
+            }
+
+        });
+        adb.setNegativeButton("Cancel", null);
+        adb.setTitle("Choose a type:");
+        adb.show();
+    }
+
+    private void showMenu() {
+        AlertDialog.Builder adb = new AlertDialog.Builder(this);
+        final CharSequence items[] = new CharSequence[]{
+                "Import image",
+                "Import demo",
+                "Reset image",
+                "Export image",
+        };
+
+        adb.setItems(items, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int n) {
+                switch (n) {
+                    case 0:
+                        importImage();
+                        break;
+                    case 1:
+                        importImageDemo();
+                        break;
+                    case 2:
+                        reset();
+                        break;
+                    case 3:
+                        exportImage();
+                        break;
+                }
+                dialog.dismiss();
+            }
+
+        });
+        adb.setNegativeButton("Cancel", null);
+        adb.setTitle("Choose a type:");
+        adb.show();
+    }
+
+    private void hideList() {
+    }
+
+    private void hideAll() {
+        sliderCon.setVisibility(View.GONE);
+
+        hideList();
+    }
+}
